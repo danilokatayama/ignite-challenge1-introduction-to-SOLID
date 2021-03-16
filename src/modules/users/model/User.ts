12bf696ff560e206643577651abcc55a62828105
Danilo Katayama <danilokatayama@gmail.com>
2021-03-16 09:44:27 -0300

    challenge finished

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,28 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+
+  name: string;
+
+  admin: boolean;
+
+  email: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      const date = new Date();
+
+      this.id = uuidV4();
+      this.admin = false;
+      this.created_at = date;
+      this.updated_at = date;
+    }
+  }
 }
 
 export { User };
